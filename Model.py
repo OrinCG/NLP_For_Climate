35a21ba2e0a8fa81e43baa7d504589b1d9af1417
ocg1g20 <ocg1g20@soton.ac.uk>
2022-05-30 16:34:51 +0100

    Added in reading a climate change snippets dataset for Model.py

--- Model.py
+++ Model.py
@@ -1,14 +1,44 @@
+import sys
+
 import fasttext
+import pandas
+
 # from gensim.models.fasttext import FastText
+
+# -*- coding: utf-8 -*-
+""" Model class:
+
+The Model class here is designed to set up the model (training, theorectically
+cleaning of data, etc) and return it as needed. At the moment it is using fasttext
+to train a supervised and unsupervised model on 
+"""
 
 
 class Model:
-    def generateModel(self):
-        fasttext.train_unsupervised()
+
+    def generateUnSupModel(self):
+        climate_opin_csv = pandas.read_csv("BBCNEWS.201701.csv")
+        snippets = climate_opin_csv["Snippet"]
+        half = int(len(snippets)/2)
+        snippets_t = snippets.iloc[:half,]
+        self.snippets_v = snippets.iloc[half:len(snippets) - 1,]
+        snippets_t.to_csv("training.txt", sep="\n", index=False)
+        return fasttext.FastText.train_unsupervised("training.txt")
 
     def __init__(self):
-        self.model = self.generateModel()
+        self.snippets_v = None
+        self.model = self.generateUnSupModel()
 
     def getModel(self):
         return self.model
 
+    def main(self) -> None:
+        print(self.generateUnSupModel())
+
+
+if __name__ == '__main__':
+    m1 = Model()
+    m1.main()
+    print(m1.model.words)
+    print(m1.model.get_nearest_neighbors("climate"))
+#    print(similarities.)
