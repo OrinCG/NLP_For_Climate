227848fa4608a1f31f474e503b4aa6a2e0204981
ocg1g20 <ocg1g20@soton.ac.uk>
2022-06-01 16:40:57 +0100

    Tested the supervised version of the model without any tuning. It is giving very generic predictions at the moment which is to be expected.

--- Model.py
+++ Model.py
@@ -1,4 +1,5 @@
 import sys
+from itertools import starmap
 
 import fasttext
 import nltk
@@ -7,6 +8,8 @@ import pandas
 # from gensim.models.fasttext import FastText
 
 # -*- coding: utf-8 -*-
+from gensim.parsing import remove_stopwords
+
 """ Model class:
 
 The Model class here is designed to set up the model (training, theorectically
@@ -17,16 +20,27 @@ to train a supervised and unsupervised model on
 
 class Model:
 
+    def combine(self,x,y):
+        splitX = x.split(",")
+        combinedS = ""
+        for label in splitX:
+            combinedS += "__label__" + label  + " "
+        return combinedS + y
+
     def generateUnSupModel(self):
         climate_opin_csv = pandas.read_csv("archive/BBCNEWS.201701.csv")
-        snippets = climate_opin_csv[["Snippet","Categories"]]
+        snippets = climate_opin_csv["Snippet"].apply(lambda x: remove_stopwords(x))
+        labels = climate_opin_csv["Categories"].dropna()
         print(snippets)
         half = int(len(snippets)/2)
         snippets_t = snippets.iloc[:half,]
+        label_snip = pandas.DataFrame(list(starmap(self.combine, zip(labels, snippets_t))))
+
         self.snippets_v = snippets.iloc[half:len(snippets) - 1,]
-        snippets_t.to_csv("training.txt", sep="\n", index=False)
+        label_snip.to_csv("training.txt", sep="\n", index=False)
         #Add tokenising and stuff here
-        return fasttext.FastText.train_unsupervised("training.txt")
+        #fasttext.supervised("training.txt","model.txt")
+        return fasttext.FastText.train_supervised("training.txt")
 
     def __init__(self):
         self.snippets_v = None
@@ -43,6 +57,10 @@ if __name__ == '__main__':
     m1 = Model()
     m1.main()
     print(m1.model.words)
-    print(m1.model.get_nearest_neighbors("climate"))
+    print(m1.model.predict("Environmentalists warn new power plant could have knock on effects on global warming.",5))
     print(m1.model.labels)
+    print("-----------------------------------------------------------------------------------------")
+    for prediction in m1.snippets_v:
+        print(prediction)
+        print(m1.model.predict(prediction,3))
 #    print(similarities.)
